330d27c96e35ceb143af34c7e72dcd1d8bcbb884
Nstampfli <nathan.stampfli@hetic.net>
2021-07-21 22:16:50 +0200

    create PoepleChart component

---
+++ src/components/chartCard/PeopleChart.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Card from "../Card";
+import { Line } from 'react-chartjs-2';
+
+const data = {
+  labels: ['9h', '10h', '11h', '12h', '13h', '14h', '15h', '16h', '17h', '18h'],
+  datasets: [
+    {
+      data: [12, 19, 13, 14, 8, 21, 17, 15, 11, 3],
+      fill: false,
+      backgroundColor: '#F0544F',
+      borderColor: '#F0544F20',
+    },
+  ],
+};
+  
+const options = {
+  plugins: {
+    legend: {
+      display: false,
+    }
+  },
+  scales: {
+    yAxis: {
+      ticks: {
+        beginAtZero: true,
+        callback: (value: number) => `${value} p.`, 
+      }
+    }
+  }
+}
+
+export default function PeopleChart() {
+  return (
+    <Card title='Nombre de personnes moyenne par salle' label="Salle C102" isCalendar>
+      <Line data={data} options={options} />
+    </Card>
+  );
+}
