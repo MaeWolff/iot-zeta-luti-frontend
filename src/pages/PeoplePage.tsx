e14da34be3512d43aa4fd36b6f77d1ce01f82ace
Nstampfli <nathan.stampfli@hetic.net>
2021-07-23 11:33:29 +0200

    add PeopleMap

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -2,7 +2,7 @@ import ComposedCard from "../components/cards/ComposedCard";
 import PeopleChart from "../components/chartCard/PeopleChart";
 import AuthenticatedRoute from "../layouts/AuthenticatedRoute";
 import GridLayout from "../layouts/GridLayout";
-import MapTemp from "../assets/map-temp.jpg";
+import PeopleMap from "../assets/svg/PeopleMap";
 
 export default function PeoplePage() {
   return (
@@ -13,7 +13,7 @@ export default function PeoplePage() {
           label="270 en ce moment"
           isCalendar
         >
-          <img src={MapTemp} alt="mapTemp" />
+          <PeopleMap />
         </ComposedCard>
 
         <PeopleChart />
